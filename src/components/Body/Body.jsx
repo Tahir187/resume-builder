c4dd4223ac3f3613a37609aa29927cc4cfc54384
Muhammad Tahir <81794146+Tahir187@users.noreply.github.com>
2023-08-12 14:32:02 +0500

    added some functionality

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from "../Body/Body.module.css"
 import { ArrowDown } from 'react-feather'
 import Edtior from '../Editor/Edtior'
+import { useState } from 'react'
 
 const Body = () => {
     const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"]
@@ -14,6 +15,48 @@ const Body = () => {
       summary: "Summary",
       other: "Other",
     };
+
+    const [resumeInformation, setResumeInformation] = useState({
+      [sections.basicInfo]:{
+        id: sections.basicInfo,
+        sectionTitle: sections.basicInfo,
+        detail:{}
+      },
+      [sections.workExp]:{
+        id: sections.workExp,
+        sectionTitle: sections.workExp,
+        details:[],
+      },
+      [sections.project]:{
+        id: sections.project,
+        sectionTitle: sections.project,
+        details:[]
+      },
+      [sections.education]:{
+        id: sections.education,
+        sectionTitle: sections.education,
+        details:[]
+      },
+      [sections.achievement]:{
+        id: sections.achievement,
+        sectionTitle: sections.achievement,
+        points: []
+      },
+      [sections.summary]:{
+        id: sections.summary,
+        sectionTitle: sections.summary,
+        detail: ''
+      },
+      [sections.other]:{
+        id: sections.other,
+        sectionTitle: sections.other,
+        detail: ""
+      },
+    });
+
+    useEffect(()=>{
+      console.log(resumeInformation);
+    },[resumeInformation])
 
   return (
     <div className={styles.container}>
@@ -28,7 +71,8 @@ const Body = () => {
         <button>Download <ArrowDown/> </button>
     </div>
     <div className={styles.main}>
-      <Edtior sections = {sections}/>
+      <Edtior sections = {sections} information ={resumeInformation} setInformation = {setResumeInformation } />
+              
     </div>
     </div>
   )
